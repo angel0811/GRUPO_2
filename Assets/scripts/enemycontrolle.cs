6234bc03d419344e151be37c76a1a0ee36a5550b
angel0811 <105165878+angel0811@users.noreply.github.com>
2026-04-05 14:47:57 -0400

    Se agrego fondo y codigo
    se agrego el fondo con animacion y codigo para dar seguimiento al personaje con la camara, solo queda agregar mas enemigos y varios objetos mas los cuales seran agregados por mis compañera.

--- Assets/scripts/enemycontrolle.cs
+++ Assets/scripts/enemycontrolle.cs
@@ -15,30 +15,28 @@ public class enemycontrolle : MonoBehaviour
 
     void Update()
     {
-        // 1. Calculamos la distancia
+        
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-        // 2. Si está cerca, calculamos dirección
         if (distanceToPlayer < detectionRadius)
         {
             Vector2 direction = (player.position - transform.position).normalized;
-            // Solo nos movemos en X
+           
             movement = new Vector2(direction.x, 0);
         }
         else
         {
-            // Si no está cerca, se queda quieto
             movement = Vector2.zero;
         }
     }
 
     void FixedUpdate()
     {
-        // 3. Aplicamos el movimiento (Aquí es donde suele faltar el paréntesis)
+        
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 
-    // Esto es para que puedas ver el radio de detección en la escena
+   
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
